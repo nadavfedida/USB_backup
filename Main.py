8a4601e48bce918b637caf34c7f8ee0254d3ff9f
Nadav Fedida <nadavz_@hotmail.com>
2022-09-20 17:08:26 +1000

    Nadav Approved

--- Main.py
+++ Main.py
@@ -1,40 +1,36 @@
 import os, shutil
 from os.path import isfile, join
-
+from os import path
+ 
 source_dir = '/Users/nadav/Desktop/Uni/Private/USB_project/Test1/'
-dest_dir = '/Users/nadav/Desktop/Uni/Private/USB_project/Test2/'
+dest_dir = '/Volumes/NadavUSB'
 
-if len(os.listdir(source_dir)) != 0:
-    print("folder 1 entered")
-    os.chdir(source_dir)
-    # f=open('testingFile1.txt','w+')
-    # f.write('this is a test doc 1')
-    
-    onlyfiles = [f for f in os.listdir(source_dir) if isfile(join(source_dir, f))]
-    print(onlyfiles)
+option = None
+try:
+    if len(os.listdir(source_dir)) != 0:
+        print("folder 1 entered")
+        os.chdir(source_dir)
+        # f=open('testingFile1.txt','w+')
+        # f.write('this is a test doc 1')
+        
+        #USB files
+        DestFiles = [f for f in os.listdir(dest_dir) if isfile(join(dest_dir, f))]
+
+        #PC files
+        SourceFiles = [f for f in os.listdir(source_dir) if isfile(join(source_dir, f))]
+
+        print(DestFiles)
+        print(SourceFiles)
+
+        for position, file in enumerate(SourceFiles):
+            print(path.exists(file))
+            if path.exists(file):
+                file_sr = SourceFiles[position]
+                shutil.copy(file_sr, dest_dir) 
+
+    else:
+        print("current directory not found")
+except Exception as E:
+    print(E)
 
 
-    if os.path.isdir(dest_dir):
-        shutil.move(os.path.join(source_dir, 'testingFile.txt'), dest_dir)
-    # f.close()
-elif len(os.listdir(dest_dir) ) != 0:
-    print("folder 2 entered")
-    os.chdir(dest_dir)
-
-
-    # f=open('testingFile.txt','w+')
-    # f.write('this is a test doc')
-    if os.path.isdir(source_dir):
-        shutil.move(os.path.join(dest_dir, 'testingFile.txt'), source_dir)
-    # f.close()
-
-else:
-    print("current directory not found")
-
-
-
-# for position, file in enumerate(destFile):
-#     if not os.path.exists(file):
-#         file_sr = srcFile[position]
-#         if not os.path.exists(file): 
-#             shutil.copy(file_sr, 'C:\destFile')
